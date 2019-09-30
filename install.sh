1406f928ee46fb6f651d290545b23113bda5be9a
Eduard Canet <ecanet@localhost.localdomain>
2019-09-30 09:55:33 +0200

    ldapserver19

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install ldap server
+
+rm -rf /etc/openldap/slapd.d/*
+rm -rf /var/lib/ldap/*
+cp DB_CONFIG /var/lib/ldap/.
+slaptest -f /opt/docker/slapd-edt.org.conf -F /etc/openldap/slapd.d
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio_edt.org.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris_edt.org.ldif 
+chown -R ldap.ldap /etc/openldap/slapd.d
+chown -R ldap.ldap /var/lib/ldap
+cp /opt/docker/ldap.conf /etc/openldap/.
+
