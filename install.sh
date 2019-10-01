3a63ddc040b4124588fb94b32ab51739b0671ce1
Eduard Canet <ecanet@localhost.localdomain>
2019-10-01 10:48:36 +0200

    ldapserver:base

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
-cp DB_CONFIG /var/lib/ldap/.
+cp /opt/docker/DB_CONFIG /var/lib/ldap/.
 slaptest -f /opt/docker/slapd-edt.org.conf -F /etc/openldap/slapd.d
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio_edt.org.ldif
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris_edt.org.ldif 
