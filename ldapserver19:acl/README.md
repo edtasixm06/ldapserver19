7c10625370c67f28a5dacdc1db5fc122f1d1c60c
Eduard Canet <ecanet@localhost.localdomain>
2019-10-14 09:48:53 +0200

    ldapserver:acl

---
+++ ldapserver19:acl/README.md
@@ -0,0 +1,8 @@
+# ldapserver19:base
+## ASIX M06-ASO @edt Curs 2019-2020
+
+Servidor multi ldap, amb organització dc=edt,dc=org, 
+i també dc=em06,dc=cat
+
+
+
---
+++ ldapserver19:acl/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Install ldap server
+
+rm -rf /etc/openldap/slapd.d/*
+rm -rf /var/lib/ldap/*
+cp /opt/docker/DB_CONFIG /var/lib/ldap/.
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.ldif
+chown -R ldap.ldap /etc/openldap/slapd.d
+chown -R ldap.ldap /var/lib/ldap
+cp /opt/docker/ldap.conf /etc/openldap/.
